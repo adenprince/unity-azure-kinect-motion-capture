4fa3e85301b6c7a094995bcea150278aa4a95ecd
adenprince <45126168+adenprince@users.noreply.github.com>
2020-12-05 18:46:53 -0600

    Added buttons for switching between data recording and playback

---
+++ Assets/Scripts/SceneSwitcher.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour
+{
+    public string newSceneName;
+
+    public void switchScene()
+    {
+        SceneManager.LoadScene(newSceneName, LoadSceneMode.Single);
+    }
+}
