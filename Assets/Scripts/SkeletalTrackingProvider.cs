3ae929470bd22e5b37170401341d0bba0c193e57
adenprince <45126168+adenprince@users.noreply.github.com>
2021-09-27 14:56:09 -0500

    Added fixes from Azure Kinect Sample Unity Body Tracking Application pull request
    Fixes come from: https://github.com/microsoft/Azure-Kinect-Samples/pull/62

--- Assets/Scripts/SkeletalTrackingProvider.cs
+++ Assets/Scripts/SkeletalTrackingProvider.cs
@@ -60,7 +60,7 @@ public class SkeletalTrackingProvider : BackgroundDataProvider
 
                 var deviceCalibration = device.GetCalibration();
 
-                using (Tracker tracker = Tracker.Create(deviceCalibration, new TrackerConfiguration() { ProcessingMode = TrackerProcessingMode.Gpu, SensorOrientation = SensorOrientation.Default }))
+                using (Tracker tracker = Tracker.Create(deviceCalibration, new TrackerConfiguration() { ProcessingMode = TrackerProcessingMode.Cuda, SensorOrientation = SensorOrientation.Default }))
                 {
                     UnityEngine.Debug.Log("Body tracker created.");
                     userMessages.queueMessage("Body tracker created.");
